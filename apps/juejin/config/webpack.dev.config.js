173c546b52f144fdf941760a6075d18cc28f8e96
yanlele <331393627@qq.com>
2024-04-27 21:46:40 +0800

    feat: *

--- apps/juejin/config/webpack.dev.config.js
+++ apps/juejin/config/webpack.dev.config.js
@@ -2,9 +2,24 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/background/index.js",
+  devtool: false,
+  entry: {
+    ["background/index"]: "./src/background/index.ts",
+  },
   output: {
-    filename: "background/name.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "../dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
 };
--- apps/juejin/src/background/index.js
+++
@@ -1,1 +0,0 @@
-console.log(`[yanle] - 123`);
