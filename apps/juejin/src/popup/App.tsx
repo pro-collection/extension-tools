ef9627f7ecf1652c6fa3c763a2d57d6a603092a2
yanlele <331393627@qq.com>
2024-04-28 14:25:06 +0800

    feat: 引入 antd 到项目里面来

--- apps/juejin/src/popup/App.tsx
+++ apps/juejin/src/popup/App.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import "./style.css";
-// import imgOne from "@src/popup/imgs/01.png";
+import { Button } from "antd";
 
 const App: React.FC = () => {
+  const handleClick = () => {
+    console.log(`[yanle] - click`);
+  };
+
   return (
     <div>
       <div className="p-1 text-red-700">hello yanlele</div>
       <div className="text-base p-1 border border-black border-solid hover:border-red-500">guang</div>
-      {/* <img src={imgOne} alt="" /> */}
+      <Button onClick={handleClick}>click</Button>
     </div>
   );
 };
