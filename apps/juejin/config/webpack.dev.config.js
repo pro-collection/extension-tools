34ee9f9c730d2335ca8ce98ef1ec20ef6b993934
yanlele <331393627@qq.com>
2024-04-28 11:48:34 +0800

    feat: 构建环境

--- apps/juejin/config/webpack.dev.config.js
+++ apps/juejin/config/webpack.dev.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,6 +8,7 @@ module.exports = {
   entry: {
     ["background/index"]: "./src/background/index.ts",
     ["scripts/index"]: "./src/scripts/index.ts",
+    ["popup/index"]: "./src/popup/index.tsx",
   },
   output: {
     filename: "[name].js",
@@ -15,14 +17,30 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: "ts-loader",
+        test: /.(jsx?)|(tsx?)$/,
         exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                // {
+                //   targets: "iOS 9, Android 4.4, last 2 versions, > 0.2%, not dead", // 根据项目去配置
+                //   useBuiltIns: "usage", // 会根据配置的目标环境找出需要的polyfill进行部分引入
+                //   // corejs: 3, // 使用 core-js@3 版本
+                // },
+              ],
+              ["@babel/preset-typescript"],
+              ["@babel/preset-react"],
+            ],
+          },
+        },
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".mjs", ".js", ".json", ".jsx", ".ts", ".tsx"], // 如果项目中只有 tsx 或 ts 可以将其写在最前面
     mainFiles: ["main", "index"], // 添加'main'作为一个默认解析的文件名
     alias: {
       "@src": path.resolve(__dirname, "../src/"),
@@ -35,6 +53,11 @@ module.exports = {
         { from: path.resolve(__dirname, "../src/images"), to: path.resolve(__dirname, "../dist/images") },
         { from: path.resolve(__dirname, "../src/style"), to: path.resolve(__dirname, "../dist/style") },
       ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../src/popup/index.html"), // 使用自定义模板
+      filename: "popup/index.html",
+      chunks: ["popup/index"],
     }),
   ],
   output: {
---
+++ apps/juejin/src/popup/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App: React.FC = () => {
+  return <div>hello yanlele</div>;
+};
+
+export default App;
