26d8f1cb3bb07a60211f427622cd0108ac1d5885
yanlele <331393627@qq.com>
2024-05-15 22:03:45 +0800

    feat: 通过注入脚本的方式, 拿到草稿信息

--- apps/juejin/src/background/getDraftContent/fetchGetContent.ts
+++ apps/juejin/src/background/getDraftContent/fetchGetContent.ts
@@ -1,16 +1,53 @@
+// import { forEach, get } from "lodash";
+
 /**
  * 通过请求来注入到 content script 里面的脚本， 用户获取 草稿文本内容
- * @param url
- * @param params
  * @returns
  */
-const fetchGetContent = (url: string, params: URLSearchParams) => {
+const fetchGetContent = () => {
+  const cookieString = document.cookie || "";
+
+  const cookiesObj: Record<string, string> = {};
+  const cookiesArray = cookieString.split(";") || [];
+
+  cookiesArray.forEach((cookie) => {
+    const list = cookie?.trim()?.split?.("=");
+    const name = list?.[0];
+    const value = list?.[1];
+    cookiesObj[name] = value;
+  });
+
+  // const tokens = get(cookiesObj, "__tea_cookie_tokens_2608", "");
+  const tokens = cookiesObj?.__tea_cookie_tokens_2608 || "";
+
+  let result;
+  try {
+    result = JSON.parse(decodeURIComponent(decodeURIComponent(tokens)));
+  } catch (e) {
+    result = {};
+  }
+
+  // const uuid = get(result, "user_unique_id", "");
+  const uuid = result?.user_unique_id || "";
+
+  const paramsObject: Record<string, string> = {
+    aid: "2608",
+  };
+
+  if (uuid) paramsObject.uuid = uuid;
+
+  const params = new URLSearchParams(paramsObject);
+
+  const reqUrl = `https://api.juejin.cn/content_api/v1/article_draft/detail?${params}`;
+
+  const pagePathName = location?.pathname || "";
+
   let draftId = "";
 
   // 正则表达式来匹配路径最后一个非数字部分
   const regex = /\/([\w-]+)$/;
 
-  const match = url.match(regex);
+  const match = pagePathName.match(regex);
 
   if (match) {
     draftId = match[1];
@@ -20,41 +57,44 @@ const fetchGetContent = (url: string, params: URLSearchParams) => {
 
   if (!draftId) return "";
 
-  return () => {
-    return fetch(`https://api.juejin.cn/content_api/v1/article_draft/detail?${params}`, {
-      headers: {
-        accept: "*/*",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "content-type": "application/json",
-        priority: "u=1, i",
-        "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site",
-        // "x-secsdk-csrf-token":
-        //   "0001000000018c2c02377ce4f96a7e60be8d4c6a62fb33f25da327725a62de2714044800f53a17cecd0d2548fb1d",
-        origin: "https://juejin.cn",
-        referrer: "https://juejin.cn/",
-      },
+  const body = JSON.stringify({
+    draft_id: draftId,
+  });
+
+  return fetch(reqUrl, {
+    headers: {
+      accept: "*/*",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+      "content-type": "application/json",
+      priority: "u=1, i",
+      "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"macOS"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site",
+      // "x-secsdk-csrf-token":
+      //   "0001000000018c2c02377ce4f96a7e60be8d4c6a62fb33f25da327725a62de2714044800f53a17cecd0d2548fb1d",
+      origin: "https://juejin.cn",
       referrer: "https://juejin.cn/",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: `{"draft_id":"${draftId}"}`,
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
+    },
+    referrer: "https://juejin.cn/",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: body,
+    // body: '{"draft_id":"7297130301288923171"}',
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+  })
+    .then((res) => {
+      return res.json();
     })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        return res;
-      })
-      .catch((e) => {
-        console.log(`[yanle] - request error e`, e);
-      });
-  };
+    .then((res) => {
+      return res;
+    })
+    .catch((e) => {
+      console.log(`[yanle] - request error e`, e);
+    });
 };
 
 export default fetchGetContent;
--- apps/juejin/src/background/getDraftContent/index.ts
+++ apps/juejin/src/background/getDraftContent/index.ts
@@ -7,7 +7,7 @@ import fetchGetContent from "./fetchGetContent";
  * @param urls 请求链接组
  */
 const getDraftContent = async (urls: string[]) => {
-  // const urlParams = await getUrlParams();
+  const urlParams = await getUrlParams();
 
   // 通过创建 tabs 的方式来进行获取文件
   // const win = await chrome.windows.create({
@@ -52,56 +52,15 @@ const getDraftContent = async (urls: string[]) => {
     selected: false,
   });
 
-  // cosnt func = fetchGetContent(tab?.url || (tab?.pendingUrl as string), urlParams);
-
   const [res] = await chrome.scripting.executeScript({
     target: { tabId: tab?.id as number },
-    func: () => {
-      return fetch(
-        "https://api.juejin.cn/content_api/v1/article_draft/detail?aid=2608&uuid=7203748436654097955",
-        {
-          headers: {
-            accept: "*/*",
-            "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-            "content-type": "application/json",
-            priority: "u=1, i",
-            "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"macOS"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-site",
-            // "x-secsdk-csrf-token":
-            //   "0001000000018c2c02377ce4f96a7e60be8d4c6a62fb33f25da327725a62de2714044800f53a17cecd0d2548fb1d",
-            origin: "https://juejin.cn",
-            referrer: "https://juejin.cn/",
-          },
-          referrer: "https://juejin.cn/",
-          referrerPolicy: "strict-origin-when-cross-origin",
-          body: '{"draft_id":"7297130301288923171"}',
-          method: "POST",
-          mode: "cors",
-          credentials: "include",
-        }
-      )
-        .then((res) => {
-          return res.json();
-        })
-        .then((res) => {
-          console.log(`[yanle] - res`, res);
-          return res;
-        })
-        .catch((e) => {
-          console.log(`[yanle] - request error e`, e);
-        });
-    },
+    func: fetchGetContent,
     injectImmediately: true,
   });
 
-  console.log(`[yanle] - res`, res);
   console.log(`[yanle] - res`, res?.result);
 
-  await chrome.tabs.remove(tab?.id as number);
+  // await chrome.tabs.remove(tab?.id as number);
 };
 
 export default getDraftContent;
